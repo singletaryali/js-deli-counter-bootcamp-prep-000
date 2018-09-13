4c58514a377bb5630f2210ac6ed46bbe21a18bfd
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-09-13 03:15:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine (line) {
   var array = [];
   for (var i= 0; i < line.length; i++) {
   array.push(` ${i+1}. ${line[i]}`); 
+  }
   return ("The line is currently:" +array);
   }
-}
 }
