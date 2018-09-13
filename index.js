bdb1bb8b55616337a9301b5ed563974c05c1be72
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-09-13 03:11:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function currentLine (line) {
  if (line.length === 0) {
    return "The line is currently empty";}
  else { 
-  var array = []
+  var array = [];
   for (var i= 0; i< line.length; i++){
   array.push(` ${i+1}. ${line[i]}`); 
-  return ("The line is currently: ${line} ")
+  return ("The line is currently:" +array);
   }
 }
 }
