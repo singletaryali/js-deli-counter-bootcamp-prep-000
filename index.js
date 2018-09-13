ccd9f4d2dd1ecc6c2500e49a0292b35eeb00f84e
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-09-13 02:26:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function takeANumber (katzDeli, name) {
 }
 
 function nowServing (katzDeliLine) {
-  if (){
-    
-  }
+  if (katzDeliLine.length > 0) {
+  return `Currently serving ${katzDeliLine.shift()}.`; }
+   else {
+  return "There is nobody waiting to be served!";
+}
 }
