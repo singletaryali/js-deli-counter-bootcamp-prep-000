d12d20d011bc50fbd0ee5b4c2db8768175c5ad0f
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-09-13 03:09:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function currentLine (line) {
  if (line.length === 0) {
    return "The line is currently empty";}
  else { 
-  var
+  var array = []
   for (var i= 0; i< katzDeliLine.length; i++){
-    
+  return ("The line is currently: ${line} ")
   }
 }
 }
