29235d264197edb28e8f665962a0c0b7899abe23
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-09-13 03:28:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function takeANumber (katzDeli, name) {
- console.log ('Welcome ${name}.')
- return katzDeli.push(`${name}`);
+function takeANumber(katzDeli, name){
+  katzDeli.push(name)
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
 }
 
 function nowServing (katzDeliLine) {
